6d62aa4e0414835fa12f578beb0c259d282cf03a
ABBUGARI POOJITHA <poojithaabbugari041@gmail.com>
2025-10-05 13:35:29 -0300

    Add files via upload

---
+++ n.py
@@ -0,0 +1,8 @@
+# Program to print first n natural numbers
+
+
+n = int(input("Enter a positive number: ")) # Input from the user
+
+
+for i in range(1, n + 1):# Using a for loop to print numbers from 1 to n
+    print(i, end=" ")  # end=" " prints numbers in the same line separated by space
